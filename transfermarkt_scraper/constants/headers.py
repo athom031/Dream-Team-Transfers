aba5892b704f43a9c91e7c1ae4ca123ab929c59b
alexthomas <alexjosthomas@gmail.com>
2023-07-06 18:07:02 -0700

    return to production state

--- transfermarkt_scraper/constants/headers.py
+++ transfermarkt_scraper/constants/headers.py
@@ -7,9 +7,8 @@ raise Exception(
 	+ '\n  Edge: Search "edge://version/" and find "User Agent" field'
 	+ '\n  Safari: Preferences >> Advanced >> Show Develop Menu in menu bar >> Develop Menu >> User Agent'
 )
-USER_AGENT = '<define user_agent here and delete exception>'
 """
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+USER_AGENT = '<define user_agent here and delete exception>'
 
 # used to inform website that requests are coming from browser
 HEADERS = { 'User-Agent': USER_AGENT }
