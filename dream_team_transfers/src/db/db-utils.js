71f0dd5f4d076ff0d0d6f3425865e3f0da77ef15
Alex Thomas <thomaalex@meta.com>
2024-01-09 17:31:28 -0800

    initialized db

--- dream_team_transfers/src/db/db-utils.js
+++ dream_team_transfers/src/db/db-utils.js
@@ -4,22 +4,26 @@ const db = new PouchDB('dtt_database');
 
 export function initializeDB() {
     return db.info().then(function (info) {
-        if (info.doc_count === 0) {
-            const initialData = {
-                _id: 'dtt_data',
-                team_picked: '',
-                team_nickname: '',
-                team_budget: 0,
-                team_value: 0,
-                team_positions: {},
-                players_bought: {},
-                players_sold: {},
-            };
-            return db.put(initialData);
-        }
+      if (info.doc_count === 0) {
+        const initialData = {
+          _id: 'dtt_data',
+          team_picked: -1,
+          team_nickname: '',
+          team_budget: 0.0,
+          team_value: 0.0,
+          team_positions: {},
+          players_bought: {},
+          players_sold: {},
+        };
+        return db.put(initialData);
+      }
     });
-}
+  }
 
-export function getTeamData() {
+  export function getTeamData() {
     return db.get('dtt_data');
-}
+  }
+
+  export function getTeamPicked() {
+    return db.get('dtt_data').then((data) => data.team_picked);
+  }
