0ff79d74b16074be93b485487e8caa382e82e3ba
Alex Thomas <thomaalex@meta.com>
2024-01-18 16:46:12 -0800

    finished styling for all screens

--- dream_team_transfers/src/utils/submit-button.js
+++ dream_team_transfers/src/utils/submit-button.js
@@ -7,14 +7,29 @@ export const getSubmitButton = () => {
   background-color: ${props => props.team !== null ? PREMIER_LEAGUE_TEAM_INFOS[props.team].primary_color : '#808080'};
   color: ${props => props.team !== null ? PREMIER_LEAGUE_TEAM_INFOS[props.team].secondary_color : '#FFFFFF'};
   border: 10px solid ${props => props.team !== null ? PREMIER_LEAGUE_TEAM_INFOS[props.team].third_color : '#5A5A5A'};
-  padding: 20px 40px;
+  padding: 2vh 5vw;
   border-radius: 20px;
-  font-size: 2vw;
   font-family: 'Saira Condensed', sans-serif;
   font-weight: 900;
   letter-spacing: 4px;
   cursor: pointer;
   transition: background-color 0.3s ease, color 0.3s ease, border-color 0.3s ease;
+
+  /* Styles for laptops and larger screens */
+  @media screen and (min-width: 1024px) {
+    font-size: 2vw;
+  }
+
+  /* Styles for phones and tablets in landscape mode */
+  @media screen and (max-width: 1023px) and (orientation: landscape) {
+    font-size: 2.5vw;
+  }
+
+  /* Styles for phones and tablets in portrait mode */
+  @media screen and (max-width: 1023px) and (orientation: portrait) {
+    font-size: 3.5vw;
+  }
+
 
   &:hover {
     background-color: ${props => darken(0.1, props.team !== null ? PREMIER_LEAGUE_TEAM_INFOS[props.team].primary_color : '#808080')};
