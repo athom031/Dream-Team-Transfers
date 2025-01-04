2ca96d00109ef8aacdf70616c525320b2493241a
athom031 <alexjosthomas@gmail.com>
2025-01-03 16:17:01 -0800

    break on work for now

--- dream_team_transfers/src/App.js
+++ dream_team_transfers/src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
-
 function App() {
-  return (
-    <h1>
-      Yo Mate, Manchester!
-    </h1>
-  );
-}
+  const [teamData, setTeamData] = useState(null);
 
-export default App;
+  useEffect(() => {
+    initializeDB().then((data) => {
+      getTeamData().then(data => setTeamData(data));
+    });
+  }, []);
+
+  if (teamData === null) {
+    return <div>Loading...</div>;
+  } else if (!teamData.team_picked) {
+    return (
+      <div>
+        <h1>
+          Team not picked yet!
+        </h1>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>
+          Team picked: {teamData.team_name}
+        </h1>
+      </div>
+    );
+  }
+}
