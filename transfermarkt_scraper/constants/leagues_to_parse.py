24c4c525d7acf7afb1d58f3fe262ee76e56eeeca
athom031 <alexjosthomas@gmail.com>
2025-01-03 16:14:04 -0800

    finalize supported teams csv

--- transfermarkt_scraper/constants/leagues_to_parse.py
+++ transfermarkt_scraper/constants/leagues_to_parse.py
@@ -35,7 +35,7 @@ LEAGUES_TO_PARSE = {
     ## PREMIER LEAGUE ##
     0: {
         name: 'Premier League',
-        url: 'https://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1',
+        url: 'https://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1/plus/?saison_id=2022',
         nation: 'England',
         logo: 'https://tmssl.akamaized.net/images/logo/header/gb1.png?lm=1521104656',
         teams_to_add_elsewhere: {
@@ -47,7 +47,7 @@ LEAGUES_TO_PARSE = {
     ## CHAMPIONSHIP ##
     1: {
         name: 'Championship',
-        url: 'https://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1',
+        url: 'https://www.transfermarkt.com/championship/startseite/wettbewerb/GB2/plus/?saison_id=2022',
         nation: 'England',
         logo: 'https://tmssl.akamaized.net/images/logo/header/gb2.png?lm=1643026970',
         teams_to_add_elsewhere: {
@@ -61,7 +61,7 @@ LEAGUES_TO_PARSE = {
     },
 	## LEAGUE ONE ##
     -1: {
-        url: 'https://www.transfermarkt.com/league-one/startseite/wettbewerb/GB3',
+        url: 'https://www.transfermarkt.com/league-one/startseite/wettbewerb/GB3/plus/?saison_id=2022',
         teams_to_add_elsewhere: {
             'Plymouth Argyle': 1,
             'Ipswich Town': 1,
@@ -71,7 +71,7 @@ LEAGUES_TO_PARSE = {
 	## LA LIGA ##
     2: {
         name: 'La Liga',
-        url: 'https://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1',
+        url: 'https://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1/plus/?saison_id=2022',
         nation: 'Spain',
         logo: 'https://tmssl.akamaized.net/images/logo/header/es1.png?lm=1557051003',
         teams_to_add_elsewhere: {
@@ -82,7 +82,7 @@ LEAGUES_TO_PARSE = {
     },
     ## LA LIGA 2 ##
     -2: {
-        url: 'https://www.transfermarkt.com/laliga2/startseite/wettbewerb/ES2',
+        url: 'https://www.transfermarkt.com/laliga2/startseite/wettbewerb/ES2/plus/?saison_id=2022',
         teams_to_add_elsewhere: {
             'Granada CF': 2,
             'UD Las Palmas': 2,
@@ -92,7 +92,7 @@ LEAGUES_TO_PARSE = {
     ## BUNDESLIGA ##
     3: {
         name: 'Bundesliga',
-        url: 'https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L1',
+        url: 'https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L1/plus/?saison_id=2022',
         nation: 'Germany',
         logo: 'https://tmssl.akamaized.net/images/logo/header/l1.png?lm=1525905518',
         teams_to_add_elsewhere: {
@@ -102,7 +102,7 @@ LEAGUES_TO_PARSE = {
     },
     ## BUNDESLIGA 2 ##
     -3: {
-        url: 'https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L2',
+        url: 'https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L2/plus/?saison_id=2022',
         teams_to_add_elsewhere: {
             '1.FC Heidenheim 1846': 3,
             'SV Darmstadt 98': 3
@@ -111,7 +111,7 @@ LEAGUES_TO_PARSE = {
 	## SERIE A ##
 	4: {
         name: 'Serie A',
-        url: 'https://www.transfermarkt.com/serie-a/startseite/wettbewerb/IT1',
+        url: 'https://www.transfermarkt.com/serie-a/startseite/wettbewerb/IT1/plus/?saison_id=2022',
         nation: 'Italy',
         logo: 'https://tmssl.akamaized.net/images/logo/header/it1.png?lm=1656073460',
         teams_to_add_elsewhere: {
@@ -122,7 +122,7 @@ LEAGUES_TO_PARSE = {
 	},
 	## SERIE B ##
 	-4: {
-        url: 'https://www.transfermarkt.com/serie-b/startseite/wettbewerb/IT2',
+        url: 'https://www.transfermarkt.com/serie-b/startseite/wettbewerb/IT2/plus/?saison_id=2022',
         teams_to_add_elsewhere: {
             'Frosinone Calcio': 4,
             'Genoa CFC': 4,
@@ -132,7 +132,7 @@ LEAGUES_TO_PARSE = {
 	## LIGUE 1 ##
 	5: {
         name: 'Ligue One',
-        url: 'https://www.transfermarkt.com/ligue-1/startseite/wettbewerb/FR1',
+        url: 'https://www.transfermarkt.com/ligue-1/startseite/wettbewerb/FR1/plus/?saison_id=2022',
         nation: 'France',
         logo: 'https://tmssl.akamaized.net/images/logo/header/fr1.png?lm=1648360140',
         teams_to_add_elsewhere: {
@@ -144,7 +144,7 @@ LEAGUES_TO_PARSE = {
 	},
 	## LIGUE 2 ##
 	-5: {
-        url: 'https://www.transfermarkt.com/ligue-1/startseite/wettbewerb/FR2',
+        url: 'https://www.transfermarkt.com/ligue-1/startseite/wettbewerb/FR2/plus/?saison_id=2022',
         teams_to_add_elsewhere: {
             'Le Havre AC': 5,
             'FC Metz': 5
@@ -153,7 +153,7 @@ LEAGUES_TO_PARSE = {
 	## LIGA PORTUGAL ##
 	6: {
         name: 'Liga Portugal',
-        url: 'https://www.transfermarkt.com/liga-nos/startseite/wettbewerb/PO1',
+        url: 'https://www.transfermarkt.com/liga-nos/startseite/wettbewerb/PO1/plus/?saison_id=2022',
         nation: 'Portugal',
         logo: 'https://tmssl.akamaized.net/images/logo/header/po1.png?lm=1626110146',
         teams_to_add_elsewhere: {
@@ -164,7 +164,7 @@ LEAGUES_TO_PARSE = {
 	},
 	## LIGA PORTUGAL 2 ##
 	-6: {
-        url: 'https://www.transfermarkt.com/liga-nos/startseite/wettbewerb/PO2',
+        url: 'https://www.transfermarkt.com/liga-nos/startseite/wettbewerb/PO2/plus/?saison_id=2022',
 		teams_to_add_elsewhere: {
             'Moreirense FC': 6,
             'SC Farense': 6,
@@ -174,7 +174,7 @@ LEAGUES_TO_PARSE = {
 	## EREDIVISIE ##
 	7: {
         name: 'Eredivisie',
-        url: 'https://www.transfermarkt.com/eredivisie/startseite/wettbewerb/NL1',
+        url: 'https://www.transfermarkt.com/eredivisie/startseite/wettbewerb/NL1/plus/?saison_id=2022',
         nation: 'Netherlands',
         logo: 'https://tmssl.akamaized.net/images/logo/header/nl1.png?lm=1674743474',
         teams_to_add_elsewhere: {
@@ -185,7 +185,7 @@ LEAGUES_TO_PARSE = {
 	},
 	## KEUKEN KAMPIOEN DIVISIE ##
 	-7: {
-        url: 'https://www.transfermarkt.com/eredivisie/startseite/wettbewerb/NL2',
+        url: 'https://www.transfermarkt.com/eredivisie/startseite/wettbewerb/NL2/plus/?saison_id=2022',
         teams_to_add_elsewhere: {
             'Heracles Almelo': 7,
             'PEC Zwolle': 7,
