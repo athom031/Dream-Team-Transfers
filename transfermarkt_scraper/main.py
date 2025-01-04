cdbc721004680f65b77b5b9a48b129f618b7d07e
athom031 <alexjosthomas@gmail.com>
2025-01-03 16:15:37 -0800

    return to production state

--- transfermarkt_scraper/main.py
+++ transfermarkt_scraper/main.py
@@ -2,11 +2,11 @@ from transfermarkt_scraper.scraped_data.scrape_and_get_supported_players import 
 from transfermarkt_scraper.scraped_data.scrape_and_get_supported_teams import scrape_and_get_supported_teams
 from transfermarkt_scraper.csvs.write_csvs import write_csvs
 
-# scrape transfermarkt for supported team data from the leagues to parse
-# scrape_and_get_supported_teams()
+# scrape transfermarkt for supported team data from the leagues to parse 
+scrape_and_get_supported_teams()
 
 # scrape transfermarkt for supported player data from supported team data
-# scrape_and_get_supported_players()
+scrape_and_get_supported_players()
 
 # from scraped data build csvs DreamTeamTransfer data
 write_csvs()
