8fdb0ded650484b8943f3a7b1877a6c30e994ddd
athom031 <alexjosthomas@gmail.com>
2025-01-03 16:20:55 -0800

    styles

--- dream_team_transfers/src/components/DreamTeam/TeamRestart/TeamRestart.js
+++ dream_team_transfers/src/components/DreamTeam/TeamRestart/TeamRestart.js
@@ -13,7 +13,7 @@ function TeamRestart() {
                 const newProgress = Math.min((Date.now() - holdStart) / 3000 * 100, 100);
                 setProgress(newProgress);
                 if (newProgress === 100) {
-                    // The button has been held for 4 or more seconds
+                    // The button has been held for 3 or more seconds
                     restartTeam() // Call the teamRestart function
                         .then(() => {
                             console.log("Team should be reset now");
@@ -43,20 +43,26 @@ function TeamRestart() {
 
     return (
         <div className="team-restart">
-            <h1>Restart Your Dream Team</h1>
-            <p>
-                Restarting your Dream Team will reset all your current progress and start a new 2023 season.
-                You can choose to manage the same team or select a different Premier League team.
-            </p>
-            <p className="warning">
-                Please note that restarting your team will permanently erase all your current data and cannot be undone.
-            </p>
-            {/* Insert images or slideshow here if needed */}
-            <button className="restart-button" onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
-                Hold to Confirm Restart
-            </button>
-            <div className="progress-bar">
-                <div className="progress-bar-fill" style={{width: `${progress}%`}}></div>
+            <div className='team-restart-container'>
+
+                <h1 className='restart-title'>
+                    Restart Your Dream Team
+                </h1>
+
+                <p>
+                    Restarting your Dream Team will reset all your current progress and start a new 2023 season.
+                    You can choose to manage the same team or select a different Premier League team.
+                </p>
+                <p className="warning">
+                    Please note that restarting your team will permanently erase all your current data and cannot be undone.
+                </p>
+                {/* Insert images or slideshow here if needed */}
+                <button className="restart-button" onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
+                    Hold to Confirm Restart
+                </button>
+                <div className="progress-bar">
+                    <div className="progress-bar-fill" style={{width: `${progress}%`}}></div>
+                </div>
             </div>
         </div>
     );
