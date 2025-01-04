912d5b46a67a9801bcb9c086db32be6efe779ef4
athom031 <alexjosthomas@gmail.com>
2025-01-03 16:20:41 -0800

    ordered imports and started briefly styling navbar

--- dream_team_transfers/src/components/DreamTeam/DreamTeam.js
+++ dream_team_transfers/src/components/DreamTeam/DreamTeam.js
@@ -1,9 +1,6 @@
-
-import './DreamTeam.css';
-
-import NavBar from './NavBar/NavBar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import NavBar from './NavBar/NavBar';
 import StartingEleven from './StartingEleven/StartingEleven';
 import SquadList from './SquadList/SquadList';
 import PlayerMarket from './PlayerMarket/PlayerMarket';
@@ -11,20 +8,24 @@ import TransferSummary from './TransferSummary/TransferSummary';
 import TeamRestart from './TeamRestart/TeamRestart';
 import PageNotFound from './PageNotFound/PageNotFound';
 
+import './DreamTeam.css';
+
 function DreamTeam() {
   return (
     <BrowserRouter>
-      <NavBar/>
-      <Routes>
-        <Route path="/squad-list" element={<SquadList />} />
-        <Route path="/player-market" element={<PlayerMarket />} />
-        <Route path="/transfer-summary" element={<TransferSummary />} />
-        <Route path="/team-restart" element={<TeamRestart />} />
-        <Route path="/" element={<StartingEleven />} />
-        <Route path="/home" element={<StartingEleven />} />
-        <Route path="/starting-eleven" element={<StartingEleven />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <div className='dream-team'>
+        <NavBar/>
+        <Routes>
+          <Route path="/squad-list" element={<SquadList />} />
+          <Route path="/player-market" element={<PlayerMarket />} />
+          <Route path="/transfer-summary" element={<TransferSummary />} />
+          <Route path="/team-restart" element={<TeamRestart />} />
+          <Route path="/" element={<StartingEleven />} />
+          <Route path="/home" element={<StartingEleven />} />
+          <Route path="/starting-eleven" element={<StartingEleven />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
