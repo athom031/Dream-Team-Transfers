a533f5734d1c766e682439f2ba4c3b7d3c3a5678
athom031 <alexjosthomas@gmail.com>
2025-01-03 16:17:33 -0800

    team logos and team pictures

--- dream_team_transfers/src/components/HomePage/HomePage.js
+++ dream_team_transfers/src/components/HomePage/HomePage.js
@@ -2,8 +2,45 @@ import './HomePage.css';
 
 function HomePage() {
   return (
-    <div>
-        Hello World, this is the home page!
+    <div className='home-page'>
+
+      <div className='title'>
+        <h1>
+          Dream Team Transfers
+        </h1>
+      </div>
+
+      <div className='intro'>
+
+        <div className='concept'>
+          Concept Introduction goes here!
+        </div>
+
+        <div className='slideshow'>
+          Slide show will go here!
+        </div>
+
+      </div>
+
+      <div className='team-selection'>
+
+        <div className='team-picker'>
+          Team Picker goes here!
+        </div>
+
+        <div className='selection-summary-and-submit'>
+
+          <div className='team-summary'>
+            If team hovered on, budget and roster value will go here
+          </div>
+
+          <div className='submit-button'>
+            Final Submit button goes here
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
