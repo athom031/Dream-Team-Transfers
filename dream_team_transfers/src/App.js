1897db1c8e9ad22d94700c275bc8b7e3d5e149fe
athom031 <alexjosthomas@gmail.com>
2025-01-03 16:17:40 -0800

    team value and budgets

--- dream_team_transfers/src/App.js
+++ dream_team_transfers/src/App.js
@@ -4,6 +4,7 @@ import { initializeDB, getTeamPicked } from './db/db-utils';
 import Loading from './components/Utils/Loading';
 import HomePage from './components/HomePage/HomePage';
 import DreamTeam from './components/DreamTeam/DreamTeam';
+import Papa from 'papaparse';
 
 function App() {
   const [teamData, setTeamData] = useState(null);
@@ -11,6 +12,23 @@ function App() {
   useEffect(() => {
     initializeDB().then(() => {
       getTeamPicked().then(data => setTeamData(data));
+    });
+
+    fetch(process.env.PUBLIC_URL + '/csvs/players.csv')
+    .then(response => response.text())
+    .then(csvString => {
+      const data = Papa.parse(csvString, { header: true, dynamicTyping: true }).data;
+      // Calculate the total value for each team
+      const teamValues = data.reduce((values, player) => {
+        if (player.team_id >= 0 && player.team_id <= 19) {
+          if (!values[player.team_id]) {
+            values[player.team_id] = 0;
+          }
+          values[player.team_id] += player.player_market_value;
+        }
+        return values;
+      }, {});
+      console.log(teamValues);
     });
   }, []);
 
