770fa27dcdcd60493d866dbcdb9303c3f59a593a
athom031 <alexjosthomas@gmail.com>
2025-01-03 16:16:20 -0800

    update

---
+++ dtt_front_end/src/components/DreamTeam/DreamTeam.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DreamTeam() {
+    return (
+        <div className="dream-team">
+            <h1>Welcome to the DreamTeam!</h1>
+        </div>
+    );
+}
+
+export default DreamTeam;
---
+++ dtt_front_end/src/components/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div className="home-page">
+            <h1>Welcome to the home page!</h1>
+        </div>
+    );
+}
+
+export default HomePage;
